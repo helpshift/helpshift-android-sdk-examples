758e7fbe8c1181010063b5910dcb8841e2160195
Daniel Victor <daniel@helpshift.com>
2013-04-09 11:56:06 +0530

    Add basic implementation of helpshift sdk.

--- NotificationCountApp/src/com/example/notificationcountapp/NotificationCountActivity.java
+++ NotificationCountApp/src/com/example/notificationcountapp/NotificationCountActivity.java
@@ -2,14 +2,33 @@ package com.example.notificationcountapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.helpshift.Helpshift;
 
 public class NotificationCountActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+  Helpshift hs;
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    hs = new Helpshift(NotificationCountActivity.this);
+    // You initialize the library by calling hs.install(APPLICATION_CONTEXT, API_KEY, DOMAIN,
+    // APP_ID) in your activity's onCreate(Bundle savedInstanceState)
+    // hs.install(APPLICATION_CONTEXT, API_KEY, DOMAIN, APP_ID)
+
+    Button helpButton = (Button) findViewById(R.id.helpButton);
+    helpButton.setOnClickListener(new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          hs.showSupport(NotificationCountActivity.this);
+        }
+      });
+  }
 }
