7272804c9819bc1549408a344f207238316e969c
Daniel Victor <daniel@helpshift.com>
2013-04-13 12:09:09 +0530

    Implement basic helpshift support.

--- GCMApp/src/com/example/gcmapp/GCMActivity.java
+++ GCMApp/src/com/example/gcmapp/GCMActivity.java
@@ -2,14 +2,39 @@ package com.example.gcmapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class GCMActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import com.helpshift.Helpshift;
+import com.helpshift.Log;
+
+public class GCMActivity extends Activity {
+
+  String TAG = "GCMAppDebug";
+  Helpshift hs;
+  Button helpButton;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    hs = new Helpshift(GCMActivity.this);
+    // You initialize the library by calling hs.install(APPLICATION_CONTEXT, API_KEY, DOMAIN,
+    // APP_ID) in your activity's onCreate(Bundle savedInstanceState)
+    // hs.install(this, "<YOUR_API_KEY>", "<yourcompany>.helpshift.com", "<YOUR_APP_ID>");
+    hs.install(this,
+               "518c9d6472a68a2ab99a42c26567e24f",
+               "test.helpshift.com",
+               "test_platform_20121108053758969-8f405d202ca65d5");
+
+    helpButton = (Button) findViewById(R.id.helpButton);
+    helpButton.setOnClickListener(new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          hs.showSupport(GCMActivity.this);
+        }
+      });
+  }
 }
