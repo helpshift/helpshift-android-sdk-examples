0970a1d0795afbd502d9c631ba9d286dcd721449
Daniel Victor <daniel@helpshift.com>
2013-04-15 15:15:26 +0530

    Implement basic helpshift support.

--- HelpshiftDemo/src/com/example/helpshiftdemo/HelpshiftDemoActivity.java
+++ HelpshiftDemo/src/com/example/helpshiftdemo/HelpshiftDemoActivity.java
@@ -2,14 +2,35 @@ package com.example.helpshiftdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class HelpshiftDemoActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import com.helpshift.Helpshift;
+import com.helpshift.Log;
+
+public class HelpshiftDemoActivity extends Activity {
+
+  String TAG = "HelpshiftDemoDebug";
+  Helpshift hs;
+  Button helpButton;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    hs = new Helpshift(HelpshiftDemoActivity.this);
+    // You initialize the library by calling hs.install(APPLICATION_CONTEXT, API_KEY, DOMAIN,
+    // APP_ID) in your activity's onCreate(Bundle savedInstanceState)
+    // hs.install(this, "<YOUR_API_KEY>", "<yourcompany>.helpshift.com", "<YOUR_APP_ID>");
+
+    helpButton = (Button) findViewById(R.id.helpButton);
+    helpButton.setOnClickListener(new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          hs.showSupport(HelpshiftDemoActivity.this);
+        }
+      });
+  }
 }
